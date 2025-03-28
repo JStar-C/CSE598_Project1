f1be3f9271ced251db8a9348288084a9a4e4b39d
JStar-C <JosephCalles918@Gmail.com>
2025-03-28 14:43:19 -0400

    Initial commit

---
+++ contracts/MyToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MyNFT is ERC721 {
+
+    string private _name;
+    string private _symbol;
+    string private _message;
+
+    constructor() ERC721("mild-paprika", "ADDMADMDAA") 
+    {
+        _name = "mild-paprika";
+        _symbol = "ADDMADMDAA";
+        _message = "Build a better future with blockchain - jrcalles";
+    }
+
+}
